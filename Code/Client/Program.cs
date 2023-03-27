35062a76ce56cfbefd777d180852df93276fff07
Max Aguilar <zz30.maxzz@gmail.com>
2023-03-27 18:56:20 -0500

    Changing kestrel PORTS to IIS

--- Code/Client/Program.cs
+++ Code/Client/Program.cs
@@ -8,12 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("FoodService", client =>
 {
-	client.BaseAddress = new Uri("https://localhost:5011/");
+	client.BaseAddress = new Uri("https://localhost:44300/foodservice/");
 });
 
 builder.Services.AddHttpClient("DrinkService", client =>
 {
-	client.BaseAddress = new Uri("https://localhost:5021/");
+	client.BaseAddress = new Uri("https://localhost:44300/drinkservice/");
 });
 
 
--- Code/Gateway/Program.cs
+++ Code/Gateway/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var proxyBuilder = builder.Services.AddReverseProxy();
+// Initialize the reverse proxy from the "ReverseProxy" section of configuration
+proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
@@ -9,17 +10,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapReverseProxy();
+});
 
 app.Run();
